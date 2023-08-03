ba7b4dd08b4b88515e3908504b932f494cf934dd
bolde-detti <bolde.detti@gmail.com>
2023-08-03 23:49:20 +0200

    Update Labo

--- python/Labo/Interpolazione.py
+++ python/Labo/Interpolazione.py
@@ -4,42 +4,42 @@ import numpy as np
 #pane fuori
 class interpolazione:
     def __init__(self, x: np.array, y: np.array, sY):
-        self.x=x
-        self.y=y
-        self.sY=sY
+        self.x = x
+        self.y = y
+        self.sY = sY
         if type( self.sY ) is float or type( self.sY ) is int:
-            self.a=True
+            self.a = True
         else: 
-            self.a=False
+            self.a = False
         pass
-    def Lineare(self) -> dict:
-        if len(self.x) != len(self.y):
+    def Lineare( self ) -> dict:
+        if np.size( self.x ) != np.size( self.y ) :
             print("Dati inseriti non validi")
         else:
-            delta=len(self.x)*np.sum(self.x**2)-np.sum(self.x**2)
-            A=(np.sum(self.y)*np.sum(self.x**2)-np.sum(self.x)*np.sum(self.x*self.y))/delta
-            B=(len(self.x)*np.sum(self.x*self.y)-np.sum(self.x)*np.sum(self.y))/delta
-            sA=((np.sum(self.x**2)/delta)**0.5)*self.sY
-            sB=((len(self.x)/delta)**0.5)*self.sY
+            delta = np.size( self.x ) * np.sum( np.power( self.x , 2 ) )- np.power( np.sum( self.x ), 2 )
+            B = ( np.sum( self.y ) * np.sum( np.power( self.x , 2 ) ) - np.sum( self.x ) * np.sum( self.x * self.y ) ) / delta
+            A = ( np.size( self.x ) * np.sum( self.x * self.y ) - np.sum( self.x ) * np.sum( self.y ) ) / delta
+            sB = ( np.sqrt( np.sum( np.power(self.x , 2) )/ delta)) * self.sY
+            sA = ( np.sqrt( np.size( self.x ) / delta) ) * self.sY
             return {"A value" : A, "A error": sA, "B value": B, "B error": sB}
     def LinOrigine(self) -> dict:
-            if len(self.x) != len(self.y):
+            if np.size( self.x ) != np.size( self.y ) :
                 print("Dati inseriti non validi")
             else:
-                A = np.sum(self.x*self.y)/np.sum(self.x**2)
-                sA = self.sY/ (np.sum(self.x**2))**0.5
+                A = np.sum( self.x * self.y ) / np.sum( np.power(self.x , 2) )
+                sA = self.sY / np.sqrt( np.sum( np.power(self.x , 2) ))
                 return {"A value": A, "A error": sA}
     def Pesata(self) -> dict:
-        delta = np.sum(1/self.sY**2) * np.sum((self.x/self.sY)**2)-(np.sum(self.x/(self.sY**2))**2)
-        A = ( np.sum(self.y/self.sY**2) * np.sum((self.x/self.sY)**2) - np.sum(self.x/(self.sY**2)) * np.sum(self.x*self.y/(self.sY**2)) )/ delta
-        B = (np.sum(1/self.sY**2)*np.sum(self.x*self.y/(self.sY**2)) - np.sum(self.x/(self.sY**2)) * np.sum(self.y/(self.sY**2)))/delta
-        sA = ( np.sum((self.x/self.sY)**2)/delta )**0.5
-        sB = ( np.sum(1/self.sY**2)/delta )**0.5
-        sAB = ( np.sum(self.x/(self.sY**2))/delta )**0.5
+        delta = np.sum( 1 / np.power( self.sY , 2 ) ) * np.sum( np.power( self.x / self.sY , 2 )) - np.power( ( np.sum( self.x / np.power( self.sY , 2)) ) , 2)
+        B = ( np.sum( self.y / np.power(self.sY , 2) ) * np.sum( np.power( ( self.x / self.sY ) , 2 ) ) - np.sum( self.x / np.power(self.sY , 2)) * np.sum( self.x * self.y / np.power( self.sY , 2)) )/ delta
+        A = ( np.sum( 1 / np.power(self.sY , 2) ) * np.sum( self.x * self.y / np.power( self.sY , 2 ) ) - np.sum( self.x / np.power( self.sY , 2 ) ) * np.sum( self.y / np.power( self.sY , 2 ))) / delta
+        sB = np.sqrt( np.sum( np.power( ( self.x / self.sY ) , 2 ) ) / delta )
+        sA = np.sqrt( np.sum( 1 / np.power( self.sY , 2 ) ) / delta )
+        sAB = np.sqrt( np.sum( self.x / np.power( self.sY ,2 ) ) / delta )
         return {"A value" : A, "A error" : sA, "B value": B, "B error": sB, "covariant": sAB}
     def Selezione(self) ->dict:
         if self.a :
-            if input("inserire 1 per intersezione nell'origine, 0 per nessun vincolo\t"):
+            if int ( input("inserire 1 per intersezione nell'origine, 0 per nessun vincolo\t") ):
                return self.LinOrigine()
             else:
                return self.Lineare()
@@ -50,8 +50,8 @@ class interpolazione:
 if __name__ == '__main__':
     x=np.array([1,2,3,4,5,6])
     y=np.array([0.1,0.2,0.3,0.4,0.5,0.6])
-    # sy=np.array([0.1,0.3,0.2,0.1,1,0.3])
-    sy=0.1
+    sy=np.array([0.1,0.3,0.2,0.1,0.2,0.3])
+    #sy=0.1
     a=interpolazione(x,y,sy)
     print(a.Selezione())
     
--- python/Labo/Labo definitivo.py
+++ python/Labo/Labo definitivo.py
@@ -2,54 +2,95 @@
 import numpy as np
 import pandas as pp
 import scipy.stats as ss
-from matplotlib import pyplot as py
-import Interpolazione
+import matplotlib.pyplot as plt
+import Interpolazione as i
 
 #il file CSV deve avere come prima colonna il nome VALUE nel caso in cui sia un istogramma,
 #nella seconda colonna nel caso in cui sia un'interpolazione
 
+def gauss( x: np.array , sx : float) -> np.array:
+        n = x - np.average( x )
+        d = 2 * sx**2
+        return 1/( sx * np.sqrt( 2 * np.pi() )) * np.exp( -0.5 * np.power( n , 2 ) / d ) 
+def line ( x: np.array , A: float , B: float ) -> np.array:
+    return A*x + B
+
 class labo:
     def __init__(self):
-        self.data=pp.read_csv("/Users/andreaboldetti/Documents/projects/just-learning/python/data.csv")
+        self.data=pp.read_csv("/Users/andreaboldetti/Documents/GitHub/My_first_Repository/python/data.csv")
         self.n_col=len(self.data.columns)
         pass
-    def stdev(x : np.array):
-        if len( x ) <20:
-            a=np.sqrt( np.sum((np.mean(x)- x)**2)/len(x)-1)
+    def stdev( x : np.array ) :
+        if np.size( x ) <20:
+            a=np.sqrt( np.sum(( np.mean(x)- x )**2)/np.size(x)-1)
         else:
-            a=np.sqrt( np.sum((np.mean(x)- x)**2)/len(x) )
+            a=np.sqrt( np.sum((np.mean(x)- x)**2)/np.size(x) )
         return a
-    def goodness(self):
-        #test del chi quadro
-        if self.n_col == 1:
-            x = self.data.value.to_numpy
-            chi = np.sum(((np.mean(x)-x))**2/(labo.stdev(x))**2)
-            return ss.chi2.cdf( chi, len(x)-3)
-        elif self.n_col == 2:
-            x = self.data[ self.data.columns [0]].to_numpy
-            y = self.data.value.to_numpy
-            sy = input("Inserire valore dell'incertezza su y")
-            a=Interpolazione(x,y,sy)
-            b=a.Lineare()
-            A=b.get("A value")
-            B=b.get("B value")
-            chi= np.sum( ((y-A*x-B )/sy)**2)
-            return ss.chi2.cdf( chi, len(x)-2)
-        else:
-            x = self.data[ self.data.columns [0]].to_numpy
-            y = self.data.value.to_numpy
-            sy = self.data[ self.data.columns[2]].to_numpy
-            a=Interpolazione(x,y,sy)
-            b=a.Pesata()
-            A=b.get("A value")
-            B=b.get("B value")
-            chi= np.sum( ((y-A*x-B )/sy)**2)
-            return ss.chi2.cdf( chi, len(x)-2)
-    def graphs(self):
-        #grafico interpolazione o istogramma determinato dalla bontà dell'adattamento dei dati al valore medio o all'equazione della retta
-        print("ciao")
-a=labo()
-a.goodness()
+    def one_column( self ):
 
+        x = self.data.value.to_numpy
+        chi = np.sum(((np.mean(x)-x))**2/(labo.stdev(x))**2)
+        ss.chi2.cdf( chi, np.size(x)-3)
+        self.gaussian( x )
 
+    def two_column( self ):
 
+        x = self.data[ self.data.columns [0]].to_numpy
+        y = self.data.value.to_numpy
+        sy = input("Inserire valore dell'incertezza su y")
+        a = i.interpolazione( x , y , sy )
+        b = a.Lineare()
+        A = b.get("A value")
+        B = b.get("B value")
+        chi = np.sum( ((y-A*x-B )/sy)**2)
+        ss.chi2.cdf( chi, np.size(x)-2)
+        self.linear_regression( x , y , sy, A , B )
+
+    def three_column( self ):
+
+        x = self.data[ self.data.columns [0]].to_numpy
+        y = self.data.value.to_numpy
+        sy = self.data[ self.data.columns[2]].to_numpy
+        a = i.interpolazione( x , y , sy )
+        b = a.Pesata()
+        A = b.get("A value")
+        B = b.get("B value")
+        chi = np.sum( ((y-A*x-B )/sy)**2)
+        ss.chi2.cdf( chi, np.size(x)-2)
+        self.weighted_linear_regression( x , y , sy, A , B )
+
+    def data_analysis (self) :
+
+        if self.n_col == 1 :
+            self.one_column()
+            
+        elif self.n_col ==2 :
+            self.two_column()
+            
+        elif self.n_col ==3 :
+            self.three_column()
+            
+    def gaussian( self , vec: np.array ) :
+
+        c=self.stdev(vec)
+        plt.hist( vec , bins= np.floor(c) , density = True)
+        plt.plot( np.linspace( min(vec), max(vec)) , gauss( vec , self.stdev(vec) ) , )
+        plt.show()
+
+    def linear_regression( self , x: np.array , y: np.array, sy: float, A: float, B: float ):
+
+        plt.errorbar( x , y , sy*np.linspace( min( x ) , max( x ), np.size( x ) ))
+        plt.plot( x , line( x ,A , B))
+        plt.show()
+
+    def weighted_linear_regression( self , x: np.array , y: np.array, sy: np.array, A: float, B: float ):
+        
+        plt.errorbar( x , y , sy )
+        plt.plot( x , line( x , A , B ))
+        plt.show()
+
+    
+if __name__ == '__main__' :
+    a=labo()
+    x = np.array([ 1,2,3,4,3,2,1,2,3,2,3,2,4,5,6,0])
+    a.gaussian ( x )
