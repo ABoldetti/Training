526dbf0c340c180dea21378454256a4a69368b41
bolde-detti <bolde.detti@gmail.com>
2023-08-08 13:24:12 +0200

    JAVA

--- python/Cose interessanti/dioporcoanimale.py
+++ python/Cose interessanti/dioporcoanimale.py
@@ -1,14 +1,11 @@
 
 def to_camel_case(text):
-    #a=list(text)
-    a = [i for a,i in enumerate(text) ]
-    print(a)
-    for i in range (0, len(text),1):
-        print(i)
-        print(a)
-        if a[i] =="-" or a[i]=="_":
+    a=text.split()
+    for i in range (len(a)):
+
+        if a[i] == "-" or a[i]== "_" :
             a[i]=' '
-            a[i].upper()
+            a[i+1].upper()
             i = i - 1
     return ''.join(a)
 
