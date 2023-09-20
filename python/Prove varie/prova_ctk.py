ea65e98146739bc3340337b7ffbdb86cd0589668
bolde.detti <bolde.detti@gmail.com>
2023-09-20 17:26:29 +0200

    Js

--- python/Prove varie/prova_ctk.py
+++ python/Prove varie/prova_ctk.py
@@ -1,2 +1,20 @@
-import ctk
+import customtkinter as ctk
 
+
+class App( ctk.CTk ):
+    def __init__( self ):
+        super().__init__()
+        self.geometry("700x400+50+200")
+        self.title ("Prova")
+        self.configure( fg_color = "darkgrey")
+        self.resizable( width=True , height=True)
+
+        self.button = ctk.CTkButton( self , command=self.button )
+        self.button.grid  ( row = 0 , column = 0 , padx = 20 , pady= 40)
+        pass
+    
+    def button( self ):
+        print( "Prova" )
+
+app = App()
+app.mainloop()
