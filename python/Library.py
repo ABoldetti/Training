59cc6a444e5fabbc00ebde9b684b3bb7548c40f3
bolde-detti <bolde.detti@gmail.com>
2024-01-21 23:54:50 +0100

    Library changes (dp)

--- python/Library.py
+++ python/Library.py
@@ -25,7 +25,7 @@ class Stats:
         plt.xlabel('data')
         plt.hist( self.data , bins= int(np.ceil( 1 + 3.322 * np.log(len(self.data)))) , density = True)
         plt.show()
-    def Sturges(x:int):
+    def sturges(x:int):
         return int(np.ceil( 1 + 3.322 * np.log(x)))
     def stats(self):
         print(f""" MEAN: \t {self.mean}
@@ -43,6 +43,7 @@ class myrand:
     def TAC( f ,xmax , xmin , ymax , ymin  , N = 1000 , *args , **kwargs):
         vec = []
         while ( len(vec)<N ):
+            print(len(vec))
             x = xmin + random.random()*(xmax-xmin)
             y = ymin + random.random()*(ymax-ymin)
             if y < f(x , *args , **kwargs):
@@ -59,6 +60,10 @@ class myrand:
             vec.append(a/M)
         return vec
     
+    def inverse( finv , N , *args , **kwargs):
+        # finv is the inverse function of the CDF normalised
+        return [ finv(random.random() , *args , **kwargs) for i in range (N)]
+    
     def HoM( f , xmax , xmin , ymax , ymin , N=1000 , *args , **kwargs):
         n = 0
         A = (xmax - xmin)*(ymax - ymin )
@@ -74,23 +79,26 @@ class myrand:
         var = np.std( f( np.linspace(xmin , xmax , N) , *args , **kwargs) )
         return (xmax-xmin)*mu , abs(var)/np.sqrt(N)
     
-    def poisson( N:int = 1000, xmin:float = 0 ,  M:int  = 1000 , la:float = 20 ):
+    
+    def poisson( xmin:float = 0 ,  M:int  = 1000 , la:float = 0.03 ):
         if xmin < 0:
             print( "ERROR" )
             return 0 
         else:
             data = []
-            xmax = xmin+(la*N)
             exp = pdfs.exp
-            a = myrand.TAC( exp , xmax , xmin , exp(0) , exp(xmin) , M )
-            print('diocane')
-            for i in range( xmin , xmax , la):
+            a = myrand.inverse( expon.ppf , 1000 , 0 , 1 )
+            xmax = max(a)
+            print(a)
+            for i in np.arange( 0 , 1  , la):
                 n = 0
                 for j in a:
-                    print( j , i )
-                    if i < j < i+la:
+                    
+                    if i < j < i+((xmax-xmin)/la):
+                        print( i , j , i+la , i < j < i+la , n)
                         n+=1
                 data.append(n)
+                print(data)
             return data
 
 
@@ -112,12 +120,46 @@ class pdfs:
         else :
             return n*expon.cdf( x , loc = mu , scale = tau)
     
+class Mix:
+    def __init__(self) -> None:
+        pass
 
+    def zeros( f , xmin , xmax , L = 0.001 , *args , **kwargs):
+        if xmin*xmax<0:
+            while abs(xmin-xmax) < L:
+                temp = (xmax+xmin)/2
+                if xmax * temp < 0 :
+                    xmin = temp
+                else: xmax = temp
+        return [xmin , abs(xmax - xmin)]
+    
+
+    def L_Ratio(f, a, b, tol=1e-5 , *args , **kwargs):
+        # minimum
+        gr = (np.sqrt(5) + 1) / 2
+        A = a
+        B = b
+        while abs(b - a) > tol:
+            c = b - (b - a) / gr
+            d = a + (b - a) / gr
+            if f( c , *args , **kwargs ) < f( d , *args , **kwargs ): 
+                b = d
+            else:
+                a = c
+        if f((b+a)/2 , *args , **kwargs ) > f(A, *args , **kwargs ) and f((b+a)/2 , *args , **kwargs ) > f(B, *args , **kwargs ):
+            return (b + a) / 2
+        elif f( B , *args , **kwargs ) > f(A, *args , **kwargs ):
+            return B
+        else: return A
 
 
 
 if __name__ == '__main__':
-    plt.hist(myrand.poisson())
+    fig,ax = plt.subplots( nrows= 1 , ncols= 2)
+    ax[0].hist(myrand.poisson() , 150)
     # plt.plot ( np.linspace(-10 , 10 , 1000) , pdfs.exp(np.linspace(-10 , 10 , 1000) , 0))
     # plt.plot ( np.ones(100) , np.linspace( -2 , 2 , 100))
+    ax[1].hist(myrand.inverse( expon.ppf , 1000 , 0 , 1 ))
     plt.show()
+
+    
