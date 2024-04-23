03f70aaa2960c00d161bc83a44cbba84e42d058c
bolde-detti <bolde.detti@gmail.com>
2024-04-23 09:34:14 +0200

    Arduino

--- Arduino/try.cpp
+++ Arduino/try.cpp
@@ -1,7 +1,18 @@
-#include std.io
+#include <iostream>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout>>'hello world'
+    // code
+    int n;
+    cout << "Diocane \n";
+    cin >> n;
+    for (int i=0 ; i<n ; i++){
+        if( n%i == 0){
+            cout << "DIOCANE \n";
+            break;
+        }
+    }
+    cout <<"MADONNA PUTTANA \n";
     return 0;
 }
