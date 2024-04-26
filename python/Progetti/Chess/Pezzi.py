a27d024f1b55e527262cfb268b88a98ff2ef838b
bolde-detti <bolde.detti@gmail.com>
2024-04-26 17:19:56 +0200

    scacchi

--- python/Progetti/Chess/Pezzi.py
+++ python/Progetti/Chess/Pezzi.py
@@ -1,11 +1,15 @@
+
+# colour True is black, False is white
 class king:
-    def __init__(self,colour,pos = (7,4) if colour else (0,4)) -> None:
+    def __init__(self,colour,pos = 0) -> None:
         self.colour = colour
         self.move = False
-        if self.colour == False:
-            self.pos = ( 0 , 4 )
-        else:
-            self.pos = ( 7 , 4 )
+        if pos == 0:
+            if self.colour:
+                self.pos = ( 7 , 4 )
+            else:
+                self.pos = ( 0 , 4 )
+        else: self.pos = pos
         pass
 
     def move ( self , a ):
@@ -13,22 +17,34 @@ class king:
             if abs(i-j) >1:
                 print( TypeError , 'move is uncompatible')
                 return 0
-            else: return 1
+            else: 
+                self.pos = a
+                return 1
 
+# colour True is black, False is white
+# side True is left, False is right
 class bishop:
     def __init__(self, colour , side) -> None:
         self.colour = colour
         if self.colour == False:
-            if side == True:
-                self.pos = ( 0 , 4 )
+            a = 0
         else:
-            self.pos = ( 7 , 4 )
+            a = 7
+        
+        if side == True:
+            b = 2
+        else:
+            b = 5
+        
+        self.pos = ( a , b)
         pass
 
     def move ( self , a ):
-        for i,j in zip(a,self.pos):
-            if abs(i-j) >1:
-                print( TypeError , 'move is uncompatible')
-                return 0
-            else: return 1
+        if abs(a[0]-self.pos[0]) == abs(a[1]-self.pos[1]):
+            self.pos = a
+            return 1
+        else:
+            print( TypeError , 'move is uncompatible')
+            return 0
+
 
