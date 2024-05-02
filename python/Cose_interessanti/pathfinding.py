d1ca0e062d926242773fea3d04ebda685a6742cf
ABoldetti <a.boldetti@campus.unimib.it>
2024-05-02 21:34:55 +0200

    path

--- python/Cose_interessanti/pathfinding.py
+++ python/Cose_interessanti/pathfinding.py
@@ -35,22 +35,25 @@ def step(coord, i):
     return coord
 
 
-def start( map , i , j,cristoddio ):
+def start( map , i , j):
     a = i 
     b = j
-    cristoddio+=1
-    print(i,j)
-
-    for k in range(4):
+    cristoddio = 0
+    temp = [0,0]
+    for counter in range(len(map)**2):
+        for k in range(4):
             temp = step([a,b] , k)
-            print(temp)
+            # print(temp)
             if temp[0] >0 and temp[0]<len(map) and temp[1] >0 and temp[1]<len(map):
-                if map[temp[0],temp[1]] != inf: 
-                    continue
-                elif map[temp[0],temp[1]]>map[a,b]+1:
+                print('-------------------------------------CHECK 3--------------------------------------------')
+                print(map, '\t\t' , temp[0],temp[1] , '\t\t' , a,b)
+                if map[temp[0],temp[1]] > map[a,b]+1:
+                    cristoddio+=1
                     map[temp[0],temp[1]] = map[a,b]+1
                     print('-------------------------------------CHECK 2--------------------------------------------')
-                    map,cristoddio = start(map,a,b,cristoddio)
+                    print(map, '\t\t' , temp[0],temp[1] , '\t\t' , a,b)
+        a = temp[0]
+        b = temp[1]
     return map,cristoddio
 
 
@@ -58,7 +61,7 @@ def find_path(map):
     for i in range(len(map)):
         for j in range(len(map[i])):
             if map[i,j] == 0:
-                map,cristoddio = start(map ,i,j,0)
+                map,cristoddio = start(map ,i,j)
     print(cristoddio)
     return map
 
