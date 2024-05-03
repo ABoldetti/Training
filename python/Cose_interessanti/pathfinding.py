43491626a521ed2a072aa164463017eb8bf1dd93
bolde-detti <bolde.detti@gmail.com>
2024-05-03 07:58:19 +0200

    path

--- python/Cose_interessanti/pathfinding.py
+++ python/Cose_interessanti/pathfinding.py
@@ -1,6 +1,10 @@
 from numpy import ones, array, inf
 
-
+'''
+TODO 
+nel for usare come condizione una funzione che controlla se tutti quelli circostanti siano non inf
+fare un'altra funzione che rileva gli inf residui e gli dà il valore minimo vicino, loopare finchè tutti gli inf non se ne sono andati
+'''
 
 map = array([[inf, -1, inf, inf, inf, -1, inf, inf, inf, inf],
               [inf, 0, inf, -1, inf, -1, inf, -1, inf, inf],
@@ -12,7 +16,6 @@ map = array([[inf, -1, inf, inf, inf, -1, inf, inf, inf, inf],
               [inf, -1, -1, -1, inf, -1, inf, -1, 0, inf],
               [inf, inf, inf, inf, inf, -1, inf, -1, inf, inf],
               [inf, inf, inf, inf, inf, -1, inf, inf, inf, inf]])
-
 maze = array([[inf, inf, inf],
                   [inf, 0, inf],
                   [inf, -1, inf]])
@@ -42,18 +45,24 @@ def start( map , i , j):
     temp = [0,0]
     for counter in range(len(map)**2):
         for k in range(4):
+            #print('\t\t\t\t' , temp[0],temp[1] , '\t\t' , a,b)
             temp = step([a,b] , k)
             # print(temp)
-            if temp[0] >0 and temp[0]<len(map) and temp[1] >0 and temp[1]<len(map):
+            if temp[0] >= 0 and temp[0]<len(map) and temp[1] >= 0 and temp[1]<len(map):
                 print('-------------------------------------CHECK 3--------------------------------------------')
                 print(map, '\t\t' , temp[0],temp[1] , '\t\t' , a,b)
                 if map[temp[0],temp[1]] > map[a,b]+1:
                     cristoddio+=1
                     map[temp[0],temp[1]] = map[a,b]+1
-                    print('-------------------------------------CHECK 2--------------------------------------------')
-                    print(map, '\t\t' , temp[0],temp[1] , '\t\t' , a,b)
-        a = temp[0]
-        b = temp[1]
+        for j in range(4):
+            temp = step([a,b] , j)
+            print('-------------------------------------CHECK 4--------------------------------------------')
+            print(map, '\t\t' , temp[0],temp[1] , '\t\t' , a,b  , '\t\t' , temp[0] >0 and temp[0]<len(map) and temp[1] >0 , temp[1] >0 and temp[1]<len(map))
+            if temp[0] >=0 and temp[0]<len(map) and temp[1] >=0 and temp[1]<len(map) and map[temp[0],temp[1]]>=0:
+                print( '---------------------------------------BAZINGA---------------------------------------------')
+                a = temp[0]
+                b = temp[1]
+                break
     return map,cristoddio
 
 
