654773706d3c8e1d9ff4b50f78e20a6ada70a1ae
bolde-detti <bolde.detti@gmail.com>
2024-06-24 20:12:28 +0200

    dist_try

--- python/Progetti/pathfinding/dist_try.py
+++ python/Progetti/pathfinding/dist_try.py
@@ -1,5 +1,9 @@
-from numpy import ones, array, inf
+from numpy import inf
 from utilities import *
+
+#//def trial( a, b):
+#//    return a+b
+
 
 def arange( start , end = None , step = 1):
     if end == None:
@@ -30,69 +34,87 @@ def dist_X(map, coord_sor, coord_tmp):
     i = 0
     for i in arange( coord_tmp[0] , coord_sor[0]):
         if map[i,coord_tmp[1]] == -1:
-            return abs( coord_tmp[0] - i+1)
-    return abs( coord_tmp[0] - i+1)
+            return abs( coord_tmp[0] - i)
+    return abs( coord_tmp[0] - i)
             
 def dist_Y(map, coord_sor, coord_tmp):
     i = 0
     for i in arange( coord_tmp[1] , coord_sor[1]):
         if map[coord_tmp[0] , i] == -1:
             return abs( coord_tmp[1] - i+1)
-    return abs( coord_tmp[1] - i+1)
+    return abs( coord_tmp[1] - coord_sor[0]+1)
 
 def dist( map , coordp , coord_sor):
     coord_tmp = list(coordp)
     dist = 0
     ausy = [coord_tmp]
     list_dist = []
-
-
-    while coord_tmp != coord_sor:
+    #^----------------------------------------------------------------------------------------------------------------------
+    while coord_tmp != list(coord_sor):
         rel_d = dist_X(map, coord_sor, coord_tmp)
 
         dist += rel_d
         coord_tmp[0] = coord_tmp[0] + rel_d if coord_tmp[0] < coord_sor[0] else coord_tmp[0] - rel_d
-        #print( '-----------------------------------', coord_tmp , coord_sor, '-------------------------------------')
+        # Update the x-coordinate of coord_tmp based on the relative distance rel_d
+        # If coord_tmp[0] is less than coord_sor[0], add rel_d to coord_tmp[0], otherwise subtract rel_d from coord_tmp[0]
+
 
         rel_d = dist_Y(map, coord_sor, coord_tmp)
         
         dist += rel_d
         coord_tmp[1] = coord_tmp[1] + rel_d if coord_tmp[1] < coord_sor[1] else coord_tmp[1] - rel_d
-        #print( '-----------------------------------', coord_tmp , coord_sor, '-------------------------------------')
+        # Update the y-coordinate of coord_tmp based on the relative distance rel_d
+        # If coord_tmp[1] is less than coord_sor[1], add rel_d to coord_tmp[1], otherwise subtract rel_d from coord_tmp[1]
 
-        if len(ausy) > 2: ausy.pop( 0 )
-        ausy.append( coord_tmp )
+
+        if len(ausy) > 2: ausy.pop(0)
+        ausy.append(coord_tmp)
         if ausy[-1] == ausy[-2]:
             break
-    if coord_tmp == coord_sor:
-        #print( '-----------------------------------' , dist , '-------------------------------------', coord_tmp , coord_sor, '-------------------------------------')
+        # Check if the last two elements in ausy are the same
+        # If they are the same, it means that coord_tmp is not changing anymore and we can break out of the loop
+
+    if coord_tmp == list(coord_sor):
+        # If coord_tmp is equal to coord_sor, it means we have reached the source coordinate
         return dist
+        # Return the total distance traveled
+
     list_dist.append(dist)
     dist = 0
 
-    
-
-    while coord_tmp != coord_sor:
+    #^----------------------------------------------------------------------------------------------------------------------
+    while coord_tmp != list(coord_sor):
         rel_d = dist_Y(map, coord_sor, coord_tmp)
         
         dist += rel_d
+        
         coord_tmp[1] = coord_tmp[1] + rel_d if coord_tmp[1] < coord_sor[1] else coord_tmp[1] - rel_d
-        #print( '-----------------------------------', coord_tmp , coord_sor, '-------------------------------------')
+        
+        # Update the y-coordinate of coord_tmp based on the relative distance rel_d
+        # If coord_tmp[1] is less than coord_sor[1], add rel_d to coord_tmp[1], otherwise subtract rel_d from coord_tmp[1]
 
         rel_d = dist_X(map, coord_sor, coord_tmp)
 
         dist += rel_d
+        
+        
         coord_tmp[0] = coord_tmp[0] + rel_d if coord_tmp[0] < coord_sor[0] else coord_tmp[0] - rel_d
-        #print( '-----------------------------------', coord_tmp , coord_sor, '-------------------------------------')
+        
+        # Update the x-coordinate of coord_tmp based on the relative distance rel_d
+        # If coord_tmp[0] is less than coord_sor[0], add rel_d to coord_tmp[0], otherwise subtract rel_d from coord_tmp[0]
 
-        if len(ausy) > 2: ausy.pop( 0 )
-        ausy.append( coord_tmp )
+        if len(ausy) > 2: ausy.pop(0)
+        ausy.append(coord_tmp)
         if ausy[-1] == ausy[-2]:
             break
+        # Check if the last two elements in ausy are the same
+        # If they are the same, it means that coord_tmp is not changing anymore and we can break out of the loop
     
-    if coord_tmp == coord_sor:
-        #print( '-----------------------------------' , dist , '-------------------------------------', coord_tmp , coord_sor, '-------------------------------------')
+    if coord_tmp == list(coord_sor):
+        # If coord_tmp is equal to coord_sor, it means we have reached the source coordinate
         return dist
+        # Return the total distance traveled
+
     list_dist.append(dist)
     dist = 0
 
@@ -123,7 +145,9 @@ def pathfind( map ):
 
 
 if __name__ == '__main__':
-    print( dist_X( map , (1,1) , (0,0)))
+
+
+    print( dist_X( map , (1,0) , (0,0)))
     #print(maze)
     print( pathfind( maze ))
 
--- python/Progetti/pathfinding/utilities.py
+++ python/Progetti/pathfinding/utilities.py
@@ -10,6 +10,10 @@ map = array([[inf, -1, inf, inf, inf, -1, inf, inf, inf, inf],
               [inf, -1, -1, -1, inf, -1, inf, -1, 0, inf],
               [inf, inf, inf, inf, inf, -1, inf, -1, inf, inf],
               [inf, inf, inf, inf, inf, -1, inf, inf, inf, inf]])
+
+
 maze = array([[inf, inf, inf],
                   [inf, 0, inf],
                   [inf, -1, inf]])
+
+
