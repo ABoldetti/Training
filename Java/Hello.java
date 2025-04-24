ce2e072ada4989eab3bcc5039fc881c6af8510f3
ABoldetti <a.boldetti@campus.unimib.it>
2025-04-24 13:50:02 +0200

    adding forgotten files

--- Java/Hello.java
+++ Java/Hello.java
@@ -3,3 +3,23 @@ public class Hello {
         System.out.println("Hello world");
     }
 }
+
+public class prime{
+    public static void main(String[] args) {
+        int n = 10;
+        boolean isPrime = true;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime && n > 1) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+}
